b7cd702427878399d66832cf3b39f02a08f39d9c
jastiadirizal <kafinjastiadirizal@gmail.com>
2023-10-04 14:08:09 +0700

    add: create login feature

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{
+    
+    /**
+     * Display login page.
+     * @return Renderable
+     */
+
+    public function index()
+    {
+        return view('index');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('dashboard.index'));
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
+
+}
